11c38b880aa3807788a9e70745bc8ee2e006b897
AlexsJones <tibbar@freedommail.ch>
2017-04-18 15:11:40 +0100

    wip

---
+++ core/registry.go
@@ -0,0 +1,54 @@
+package registry
+
+import (
+	"errors"
+	"log"
+	"reflect"
+)
+
+//Entry is a utility structure for holding type information for reflection
+type Entry struct {
+	name    string
+	refType reflect.Type
+}
+
+//Unwrap brings the entry back into a real object
+func (e *Entry) Unwrap() interface{} {
+	return reflect.New(e.refType).Elem().Interface()
+}
+
+//Registry is a utility structure for holding type information for reflection
+type Registry struct {
+	storedEntries map[string]*Entry
+}
+
+//Put will register a type against a string
+func (r *Registry) Put(i interface{}) {
+	t := reflect.TypeOf(i)
+	e := &Entry{}
+	e.name = t.String()
+	e.refType = t
+	log.Println("Registering as " + t.String())
+	r.storedEntries[t.String()] = e
+}
+
+//Get will return a new instance of type if avialable
+func (r *Registry) Get(name string) (*Entry, error) {
+	e := r.storedEntries[name]
+	if e == nil {
+		return nil, errors.New("Not found")
+	}
+	return e, nil
+}
+
+//NewRegistry takes optional functions that can manipulate the object initialised
+func NewRegistry(options ...func(*Registry) error) (*Registry, error) {
+	r := &Registry{storedEntries: make(map[string]*Entry)}
+	for _, op := range options {
+		err := op(r)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return r, nil
+}
---
+++ example.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+
+	reg "github.com/AlexsJones/go-type-registry/core"
+)
+
+type foo struct {
+}
+
+func (*foo) Hello() string {
+	return "Hello"
+}
+
+func generateRegistry(r *reg.Registry) error {
+	r.Put(&foo{})
+	return nil
+}
+func main() {
+	registry, err := reg.NewRegistry(generateRegistry)
+	if err != nil {
+		fmt.Printf("error: %s\n", err.Error())
+		return
+	}
+	value, err := registry.Get("*main.foo")
+	if err != nil {
+		fmt.Printf("error: %s\n", err.Error())
+		return
+	}
+
+	unwrapped := value.Unwrap().(*foo)
+
+	fmt.Println(unwrapped.Hello())
+}
